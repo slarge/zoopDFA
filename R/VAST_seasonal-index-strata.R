0086f0158fa4f40891e52ccf13adec9e8d7aaf02
slarge <largesi@gmail.com>
2022-11-17 14:16:52 -0500

    update strata code

--- R/VAST_seasonal-index-strata.R
+++ R/VAST_seasonal-index-strata.R
@@ -3,7 +3,7 @@
 
 # install.packages('TMB', type = 'source')
 # install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
-remotes::install_github("james-thorson/VAST@main", INSTALL_opts="--no-staged-install")
+# remotes::install_github("james-thorson/VAST@main", INSTALL_opts="--no-staged-install")
 
 library(dplyr)
 library(tidyr)
@@ -18,41 +18,12 @@ library(VAST)
 # Load Data -----
 data("ecomon_strata")
 
-# # # Number of stations per year
-# total_stations <- ecomon_epu %>%
-#   group_by(year) %>%
-#   summarize(total = n_distinct(id))
-#
-# season_list <- c("winter", "spring", "summer", "fall")
-# #
-# # ## Proportion of stations with positive tows per year. Cutoff used to "keep"
-# spps <- ecomon_epu %>%
-#   mutate(present = ifelse(abundance > 0,
-#                           1, 0)) %>%
-#   filter(year >= 1994,
-#         spp != "euph1") %>%
-#   left_join(total_stations) %>%
-#   group_by(year, season, spp) %>%
-#   summarize(positive_stations = sum(present, na.rm = TRUE)/total,
-#             keep = ifelse(positive_stations > .10,
-#                           1, 0)) %>%
-#   distinct()
-# #
-# # ## List of spp where the total number of years by season is greater than 5
-# # ## and each spp group has more than cutoff of positive tows
-# spp_list <- spps %>%
-#   filter(season %in% season_list) %>%
-#   group_by(spp, season) %>%
-#   summarize(count = sum(keep)) %>%
-#   filter(all(count > 5)) %>%
-#   select(spp) %>%
-#   distinct() %>%
-#   pull(spp)
-
 spp_list <- c("calfin", "chaeto", "cham", "clauso", "ctyp",
               "euph", "gas", "hyper", "larvaceans",
               "mlucens", "oithspp", "para", "pseudo", "tlong")
 
+season_set <- c("winter", "spring", "summer", "fall")
+year_set <- 1994:2017
 
 ecomon_strata <- ecomon_strata %>%
   mutate(stock = case_when(strata %in% c(1380, 1390) ~ "EGOM",
@@ -68,10 +39,11 @@ ecomon_strata <- ecomon_strata %>%
                            TRUE ~ NA_character_)) %>%
   filter(!is.na(stock))
 
+
 # "euph1"
-# sp <- "calfin"
-# n_x <- 100
-#
+sp <- "calfin"
+n_x <- 100
+
 
 vast_wrapper <- function(sp, n_x = 100) {
 
@@ -91,40 +63,39 @@ vast_wrapper <- function(sp, n_x = 100) {
   sink(my_log, append = TRUE, type = "message")
   on.exit(sink(file = NULL), add = TRUE, after = TRUE)
 
-
   zoop_dat <- ecomon_strata %>%
     dplyr::filter(grepl(sprintf("^%s", sp) , spp),
                   stock %in% c("EGOM", "WGOM", "GB", "SNES", "WSS"),
                   as.numeric(year) >= 1994) %>%
     dplyr::mutate(areaswept_km2 = 1) %>%
-    group_by(year, season) %>%
+    # group_by(year, season) %>%
     # slice_sample(prop = .5) %>%
     # droplevels() %>%
     data.frame()
 
-  # ggplot(zoop_dat, aes(x = lon, y = lat)) +
+  # ggplot(tt, aes(x = lon, y = lat)) +
   #   geom_point(data = zoop_dat %>% filter(abundance == 0), color = "black", fill = "black", shape = 21) +
   #   geom_point(data = zoop_dat %>% filter(abundance > 0), aes(color = stock, size = abundance), alpha = 0.5) +
-  #   facet_wrap(~year) +
+  #   facet_wrap(~year_season, ncol = 4) +
   #   # labs(title = i) +
   #   NULL
+  #
 
-
-  # Load data and quick exploration of structure
-  # Set of years and seasons. The DFO spring survey usually occurs before the NOAA NEFSC spring survey, so ordering accordingly.
-  year_set = sort(unique(zoop_dat[,'year']))
-  season_set = c("winter", "spring", "summer", "fall")
-
-  # Create a grid with all unique combinations of seasons and years and then combine these into one "year_season" variable
+  # # Load data and quick exploration of structure
+  # # Set of years and seasons. The DFO spring survey usually occurs before the NOAA NEFSC spring survey, so ordering accordingly.
+  # year_set = sort(unique(zoop_dat[,'year']))
+  # season_set = c("winter", "spring", "summer", "fall")
+  #
+  # # Create a grid with all unique combinations of seasons and years and then combine these into one "year_season" variable
   yearseason_grid = expand.grid("season" = season_set, "year" = year_set)
   yearseason_levels = apply(yearseason_grid[,2:1], MARGIN = 1, FUN = paste, collapse = "_")
   yearseason_labels = round(yearseason_grid[,'year'] + (as.numeric(factor(yearseason_grid[,'season'], levels = season_set))-1)/length(season_set), digits=1)
-
-  # Similar process, but for the observations
+  #
+  # # Similar process, but for the observations
   yearseason_i = apply(zoop_dat[,c("year","season")], MARGIN = 1, FUN = paste, collapse = "_")
   yearseason_i = factor(yearseason_i, levels = yearseason_levels)
-
-  # Add the year_season factor column to our sampling_data data set
+  #
+  # # Add the year_season factor column to our sampling_data data set
   zoop_dat$year_season = yearseason_i
   zoop_dat$season = factor(zoop_dat$season, levels = season_set)
 
@@ -136,11 +107,12 @@ vast_wrapper <- function(sp, n_x = 100) {
     year = yearseason_grid[,'year'],
     season = yearseason_grid[,'season'],
     year_season = yearseason_levels,
-    lat = mean(zoop_dat[,'lat']),
-    lon = mean(zoop_dat[,'lon']),
-    areaswept_km2 = mean(zoop_dat[,'areaswept_km2']),
+    lat = mean(zoop_dat[,'lat'], na.rm = TRUE),
+    lon = mean(zoop_dat[,'lon'], na.rm = TRUE),
+    areaswept_km2 = mean(zoop_dat[,'areaswept_km2'], na.rm = TRUE),
     abundance = 0,
     dummy = TRUE)
+
 
   # Combine with sampling data
   full_data = rbind(cbind(zoop_dat, dummy = FALSE), dummy_data)
@@ -187,7 +159,6 @@ vast_wrapper <- function(sp, n_x = 100) {
 
   FieldConfig <- c("Omega1" = 1, "Epsilon1" = 1, "Omega2" = 1, "Epsilon2" = 1)
 
-
   ## Autoregressive structure -----
 
   ## Control autoregressive structure for parameters over time
@@ -201,10 +172,10 @@ vast_wrapper <- function(sp, n_x = 100) {
   ## 3 = fixed effect that is constant over time
   ## 4 = AR1 process
 
-  RhoConfig <- c("Beta1"    = c(0, 1, 2, 3, 4)[4],
-                 "Beta2"    = c(0, 1, 2, 3, 4)[4],
-                 "Epsilon1" = c(0, 1, 2, 3, 4)[5],
-                 "Epsilon2" = c(0, 1, 2, 3, 4)[5])
+  RhoConfig <- c("Beta1"    = 3,
+                 "Beta2"    = 3,
+                 "Epsilon1" = 4,
+                 "Epsilon2" = 4)
 
   ## Correlated overdispersion -----
 
@@ -228,6 +199,7 @@ vast_wrapper <- function(sp, n_x = 100) {
 
   ObsModel <- c("PosDist" = 1, # Delta-Gamma; Alternative "Poisson-link delta-model" using log-link for numbers-density and log-link for biomass per number
                 "Link"    = 1)
+  # ObsModel <-  c(10, 2)
 
 
   strata.limits <- list("EGOM" = c(1380, 1390),
@@ -282,7 +254,7 @@ vast_wrapper <- function(sp, n_x = 100) {
                        t_i = samp_dat$year_season,
                        b_i = as_units(samp_dat$abundance, "count"),
                        a_i = as_units(samp_dat$areaswept_km2, "km^2"),
-                       epu_to_use = settings$epu_to_use,
+                       # epu_to_use = settings$epu_to_use,
                        working_dir = working_dir,
                        X1config_cp = X1config_cp_use,
                        X2config_cp = X2config_cp_use,
@@ -293,14 +265,14 @@ vast_wrapper <- function(sp, n_x = 100) {
                                           # Year_Cov = contrasts(cov_dat$Year_Cov, contrasts = FALSE)
                                           ),
                        run_model = FALSE,
-                       PredTF_i = samp_dat$Dummy,
+                       PredTF_i = samp_dat$Dummy)#,
                        # Use_REML = FALSE,
                        # getsd = TRUE,
                        # test_fit = FALSE,
-                       Options = c('treat_nonencounter_as_zero' = TRUE),
+                       # Options = c('treat_nonencounter_as_zero' = TRUE),
                        # Method = Method,
-                       optimize_args = list("lower" = -Inf,
-                                            "upper" = Inf))
+                       # optimize_args = list("lower" = -Inf,
+                                            # "upper" = Inf))
 
 
   # Adjust mapping for log_sigmaXi and fitting final model -- pool variance for all seasons and then set year's to NA
@@ -330,7 +302,7 @@ vast_wrapper <- function(sp, n_x = 100) {
                   t_i = samp_dat$year_season,
                   b_i =  as_units(samp_dat$abundance, "count"),
                   a_i = as_units(samp_dat$areaswept_km2, "km^2"),
-                  epu_to_use = settings$epu_to_use,
+                  # epu_to_use = settings$epu_to_use,
                   working_dir = working_dir,
                   X1config_cp = X1config_cp_use,
                   X2config_cp = X2config_cp_use,
@@ -340,15 +312,15 @@ vast_wrapper <- function(sp, n_x = 100) {
                   X_contrasts = list(Season = contrasts(cov_dat$Season, contrasts = FALSE)#,
                                      # Year_Cov = contrasts(cov_dat$Year_Cov, contrasts = FALSE)
                                      ),
-                  newtonsteps = 1,
+                  newtonsteps = 0,
                   PredTF_i = samp_dat$Dummy,
-                  Map = Map_adjust,
+                  Map = Map_adjust)#,
                   # Use_REML = FALSE,
                   # getsd = TRUE,
                   # Options = c('treat_nonencounter_as_zero' = TRUE),
                   # Method = Method,
-                  optimize_args = list("lower" = -Inf,
-                                       "upper" = Inf))
+                  # optimize_args = list("lower" = -Inf,
+                  #                      "upper" = Inf))
 
   # Calculate predictive distribution for a Index_ctl
   index_ctl_sd_array <- sample_variable(Sdreport = fit$par$SD,
@@ -364,6 +336,6 @@ vast_wrapper <- function(sp, n_x = 100) {
 
 possibly_vast_wrapper <- purrr::quietly(vast_wrapper)
 
-# plan(multisession, workers = 4)
-vast_runs <- purrr::map(.x = spp_list, .f = possibly_vast_wrapper)
-# plan(sequential)
+# future::plan(future::multisession, workers = 4)
+vast_runs <- purrr::map(.x = spp_list[1], .f = possibly_vast_wrapper)
+# future::plan(sequential)
