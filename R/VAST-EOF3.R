0c4e8d79c7a62d6b805c180c9cfc0c78f99800a9
Scott Large <scott.large@nmfs.local>
2023-01-09 09:22:56 -0500

    clean up

--- R/VAST-EOF3.R
+++ R/VAST-EOF3.R
@@ -20,52 +20,52 @@ library(VAST)
 data("ecomon_epu")
 
 # # # Number of stations per year
-# total_stations <- ecomon_epu %>%
-#   group_by(year) %>%
-#   summarize(total = n_distinct(id))
-#
-# season_list <- c("winter", "spring", "summer", "fall")
-# #
-# # ## Proportion of stations with positive tows per year. Cutoff used to "keep"
-# spps <- ecomon_epu %>%
-#   mutate(present = ifelse(abundance > 0,
-#                           1, 0)) %>%
-#   filter(year >= 1994,
-#         spp != "euph1") %>%
-#   left_join(total_stations) %>%
-#   group_by(year, season, spp) %>%
-#   summarize(positive_stations = sum(present, na.rm = TRUE)/total,
-#             keep = ifelse(positive_stations > .10,
-#                           1, 0)) %>%
-#   distinct()
+total_stations <- ecomon_epu %>%
+  group_by(year) %>%
+  summarize(total = n_distinct(id))
+
+
+### Proportion of stations with positive tows per year. Cutoff used to "keep"
+spps <- ecomon_epu %>%
+  mutate(present = ifelse(abundance > 0,
+                          1, 0)) %>%
+  filter(year >= 1994,
+         spp != "euph1") %>%
+  left_join(total_stations) %>%
+  group_by(year, spp) %>%
+  summarize(positive_stations = sum(present, na.rm = TRUE)/total,
+            keep = ifelse(positive_stations > .1,
+                          1, 0),
+            forage_group = forage_group) %>%
+  distinct()
+
 # #
 # # ## List of spp where the total number of years by season is greater than 5
 # # ## and each spp group has more than cutoff of positive tows
-# spp_list <- spps %>%
-#   filter(season %in% season_list) %>%
-#   group_by(spp, season) %>%
-#   summarize(count = sum(keep)) %>%
-#   filter(all(count > 5)) %>%
-#   select(spp) %>%
-#   distinct() %>%
-#   pull(spp)
+spp_list <- spps %>%
+  dplyr::filter(#season %in% season_list,
+    forage_group >= 100) %>%
+  group_by(spp) %>%
+  summarize(count = sum(keep)) %>%
+  filter(count > 1) %>%
+  select(spp) %>%
+  distinct() %>%
+  pull(spp)
 
-spp_list <- c("calfin", "chaeto", "cham", "clauso", "ctyp",
-              "euph", "gas", "hyper", "larvaceans",
-              "mlucens", "oithspp", "para", "pseudo", "tlong")
+# n_x = 1000
+# td <- vast_wrapper(n_x = 500)
 
-season_list <- c("spring", "fall")[1]
+vast_wrapper <- function(n_x = 50) {
 
-vast_wrapper <- function(season = c("spring", "fall")[1], n_x = 50) {
   ## Seasonal model -----
-  working_dir <- here::here(sprintf("analysis/vast_EOF3/zoop_%s", season))
+  working_dir <- here::here("analysis/vast_EOF3/zoop_ichthyo")
 
   if(!dir.exists(working_dir)) {
     dir.create(working_dir, recursive  = TRUE)
   }
   #
   ## Attempt to create a log file
-  my_log <- file(sprintf("%s/%s_log-%s.txt", working_dir, season, n_x)) # File name of output log
+  my_log <- file(sprintf("%s/log-%s.txt", working_dir, n_x)) # File name of output log
 
   sink(my_log, append = TRUE, type = "output") # Writing console output to log file
   on.exit(sink(file = NULL), add = TRUE, after = TRUE)
@@ -77,24 +77,26 @@ vast_wrapper <- function(season = c("spring", "fall")[1], n_x = 50) {
   zoop_dat <- ecomon_epu %>%
     dplyr::filter(spp %in% spp_list,
                   EPU %in% c("GB", "GOM", "MAB"),
-                  season == !!season,
-                  as.numeric(year) >= 1994) %>%
-    dplyr::mutate(areaswept_km2 = 1) %>%
-    group_by(year, season) %>%
-    # slice_sample(prop = .5) %>%
-    # droplevels() %>%
+                  # season == !!season,
+                  as.numeric(year) >= 1999) %>%
+    dplyr::mutate(areaswept_km2 = 1,
+                  species_number = as.numeric(factor(spp)) - 1) %>%
+    select(species_number,
+           spp,
+           year,
+           abundance,
+           areaswept_km2,
+           lat,
+           lon) %>%
     data.frame()
 
-  # ggplot(zoop_dat, aes(x = lon, y = lat)) +
-  #   geom_point(data = zoop_dat %>% filter(abundance == 0), color = "black", fill = "black", shape = 21) +
-  #   geom_point(data = zoop_dat %>% filter(abundance > 0), aes(color = season, size = abundance), alpha = 0.5) +
-  #   facet_wrap(~year) +
-  #   # labs(title = i) +
-  #   NULL
-  #
-
-  # # Some last processing steps
-  zoop_dat = zoop_dat[, c("year", "lat", "lon", "areaswept_km2", "abundance")]
+#
+#   ggplot(zoop_dat, aes(x = lon, y = lat)) +
+#     geom_point(data = zoop_dat %>% filter(abundance == 0), color = "black", fill = "black", shape = 21) +
+#     geom_point(data = zoop_dat %>% filter(abundance > 0), aes(color = season, size = abundance), alpha = 0.5) +
+#     facet_wrap(~year) +
+#     # labs(title = i) +
+#     NULL
 
 
   #####
@@ -116,12 +118,12 @@ vast_wrapper <- function(season = c("spring", "fall")[1], n_x = 50) {
   ## >1 = number of elements in a factor-analysis covariance
   ## "IID" = random effect following an IID distribution
 
-  # FieldConfig <- c("Omega1" = "IID",
-  #                  "Epsilon1" = "IID", "Omega2" = "IID", "Epsilon2" = "IID")
+  FieldConfig <- c("Omega1" = "IID", "Epsilon1" = "IID",
+                   "Omega2" = "IID", "Epsilon2" = "IID")
 
-  FieldConfig_eof3 <- matrix(c("IID", "Identity", "IID", 2, 0, 0, "IID", "Identity"), ncol = 2, nrow = 4,
-                             dimnames = list(c("Omega", "Epsilon", "Beta", "Epsilon_year"),
-                                             c("Component_1", "Component_2")))
+  # FieldConfig_eof3 <- matrix(c("IID", "Identity", "IID", 2, 0, 0, "IID", "Identity"), ncol = 2, nrow = 4,
+  #                            dimnames = list(c("Omega", "Epsilon", "Beta", "Epsilon_year"),
+  #                                            c("Component_1", "Component_2")))
 
 
   ## Autoregressive structure -----
@@ -138,7 +140,7 @@ vast_wrapper <- function(season = c("spring", "fall")[1], n_x = 50) {
   ## 4 = AR1 process
 
   RhoConfig <- c("Beta1"    = c(0, 1, 2, 3, 4)[1],
-                 "Beta2"    = c(0, 1, 2, 3, 4)[4],
+                 "Beta2"    = c(0, 1, 2, 3, 4)[1],
                  "Epsilon1" = c(0, 1, 2, 3, 4)[1],
                  "Epsilon2" = c(0, 1, 2, 3, 4)[1])
 
@@ -156,7 +158,6 @@ vast_wrapper <- function(season = c("spring", "fall")[1], n_x = 50) {
   OverdispersionConfig <- c("eta1" = 0,
                             "eta2" = 0)
 
-
   ## Observation model -----
   # Control observation model structure. The first
   # component sets the distribution of the positive
@@ -171,11 +172,11 @@ vast_wrapper <- function(season = c("spring", "fall")[1], n_x = 50) {
                            strata.limits = "EPU",
                            purpose = "EOF3",
                            n_categories = 2,
-                           FieldConfig = FieldConfig_eof3,
-                           # RhoConfig = RhoConfig,
-                           ObsModel = ObsModel,
-                           bias.correct = FALSE,
-                           Options = c('treat_nonencounter_as_zero' = TRUE) )
+                           # FieldConfig = FieldConfig,
+                           RhoConfig = RhoConfig,
+                           bias.correct = TRUE,
+                           Options = c('treat_nonencounter_as_zero' = TRUE),
+                           ObsModel = ObsModel)#,
 
   settings$epu_to_use <- c("Georges_Bank", "Gulf_of_Maine", "Mid_Atlantic_Bight")
 
@@ -184,47 +185,55 @@ vast_wrapper <- function(season = c("spring", "fall")[1], n_x = 50) {
   #####
 
   fit = fit_model(settings = settings,
-                       Lat_i = zoop_dat$lat,
-                       Lon_i = zoop_dat$lon,
-                       t_i = zoop_dat$year,
-                       b_i = as_units(zoop_dat$abundance, "count"),
-                       a_i = as_units(zoop_dat$areaswept_km2, "km^2"),
-                       epu_to_use = settings$epu_to_use,
-                       working_dir = working_dir,
-                       Use_REML = TRUE,
-                       run_model = TRUE,
-                       # build_model = TRUE,
-                       test_fit = FALSE,
-                       getsd = FALSE,
-                       newtonsteps = 0,
-                       Options = c('treat_nonencounter_as_zero' = TRUE),
-                       optimize_args = list("lower" = -Inf,
-                                            "upper" = Inf))
+                  Lat_i = zoop_dat$lat,
+                  Lon_i = zoop_dat$lon,
+                  t_i = zoop_dat$year,
+                  c_i = zoop_dat$species_number,
+                  b_i = as_units(zoop_dat$abundance, "count"),
+                  a_i = as_units(zoop_dat$areaswept_km2, "km^2"),
+                  epu_to_use = settings$epu_to_use,
+                  working_dir = working_dir,
+                  Use_REML = TRUE,
+                  # run_model = FALSE,
+                  # build_model = TRUE,
+                  # test_fit = FALSE,
+                  getsd = TRUE,
+                  newtonsteps = 1,
+                  Options = c('treat_nonencounter_as_zero' = TRUE),
+                  optimize_args = list("lower" = -Inf,
+                                       "upper" = Inf))
 
+
+#
+#   results = plot( fit,
+#                   check_residuals=FALSE,
+#                   plot_set=c(3,16) )
+#
+#   results = plot( fit,
+#                   check_residuals=FALSE,
+#                   plot_set=c(2,3,6:9,11:16))
+#
+#   index1_tf = results$Factors$Rotated_loadings$EpsilonTime1
+#   cpe_vf = ecodist::vf(index1_tf, data.frame("CPE"=CPE), nperm=1000)
+#   png( "year_ordination.png", width=6, height=6, res=200, units="in")
+#   plot( index1_tf, type="n" )
+#   text( index1_tf, labels=rownames(index1_tf) )
+#   plot( cpe_vf )
+#   dev.off()
 
   saveRDS(fit, file = paste0(working_dir, "/fit.rds"))
   # saveRDS(index_ctl_sd_array, file = paste0(working_dir, "/index_ctl_sd_array.rds"))
-
+  return(fit)
 }
 
+fit <- vast_wrapper(n_x = 500)
+
+
+fit$data_list
 
 # Plot results, including spatial term Omega1
 results = plot( fit,
                 check_residuals=FALSE,
-                plot_set=c(3,16))
+                plot_set=c(3,16),
+                category_names = spp_list)
 
-
-list.files(system.file("executables",package = "VAST"))
-fit$settings$Version
-fit$input_args$model_args_input
-
-dyn.load( TMB::dynlib("VAST_v13_0_0") )
-
-fit <- readRDS(here::here(working_dir, "fit.rds"))
-fit_now <- reload_model(x = fit_orig)
-
-possibly_vast_wrapper <- purrr::quietly(vast_wrapper)
-
-# plan(multisession, workers = 4)
-vast_runs <- purrr::map(.x = season_list, .f = possibly_vast_wrapper)
-# plan(sequential)
